a7501f694610a9de42b2bff90add70c3347b06f3
Baibhab <baibhabnayak.31@gmail.com>
2024-07-31 20:34:43 +0530

    eroor corrected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import WeatherApp from './weather';
+import PlantSpace from './PlantSpace';
 
 function App() {
   return (
     <div className="App">
     <WeatherApp/>
+    
     </div>
   );
 }
